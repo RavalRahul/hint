484a01dd98cdaec845276216f2c64ea24b5dc7a2
RavalRahul <69726537+RavalRahul@users.noreply.github.com>
2020-10-10 11:01:03 +0530

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        Name&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        Post&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        salary&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;
+        <br />
+        <br />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Insert" />
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    MySqlConnection con;
+    MySqlCommand cmd;
+
+    void mycon()
+    {
+        con = new MySqlConnection("server=localhost;user id=root;database=demo");
+        con.Open();
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        mycon();
+        cmd = new MySqlCommand("insert into employee values(NULL, @emp_name, @emp_post, @emp_salary)",con);
+        cmd.Parameters.AddWithValue("@emp_name", TextBox1.Text);
+        cmd.Parameters.AddWithValue("@emp_post", TextBox2.Text);
+        cmd.Parameters.AddWithValue("@emp_salary", TextBox3.Text);
+        cmd.ExecuteNonQuery();
+        con.Close();
+    }
+}
